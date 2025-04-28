cef9fd7f5bc31ed48fc354454506879f566fd7b4
sunnyshoun <chej40505@gmail.com>
2025-04-28 23:57:41 +0800

    upload arduino serial test program

--- Arduino/SerialTest/SerialTest.ino
+++ Arduino/SerialTest/SerialTest.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+#define RX_PIN 6
+#define TX_PIN 7
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // 如果 Serial 有資料，轉發到 mySerial
+  if (Serial.available()) {
+    char c = Serial.read();
+    mySerial.write(c);
+  }
 
+  // 如果 mySerial 有資料，轉發到 Serial
+  if (mySerial.available()) {
+    char c = mySerial.read();
+    Serial.write(c);
+  }
 }
